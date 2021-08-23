252db8f5312e26faba49a081b482c04d2a706820
Yassine Bennkhay <benkhayyassine8@gmail.com>
2021-08-23 06:30:04 -0700

    remove entred address

--- lib/screens/edit_post.dart
+++ lib/screens/edit_post.dart
@@ -23,16 +23,17 @@ class _EditPostState extends State<EditPost> {
   final _formKey = GlobalKey<FormState>();
   String doc;
   XFile _userImageFile;
+  TextStyle myHintStyle= TextStyle(
+      color: Colors.black,
+      fontSize:18,
+      fontFamily: 'RobotoRegular'
+  );
   void _pickedImage(XFile image) {
     _userImageFile = image;
   }
 
   bool _isLoading = false;
   var _scaffoldKey = GlobalKey<ScaffoldState>();
-
-  /*final _titleController = TextEditingController();
-  final _priceController = TextEditingController();
-  final _descriptionController = TextEditingController();*/
 
   var _enteredTitle = '';
   var _enteredPrice = '';
@@ -46,32 +47,14 @@ class _EditPostState extends State<EditPost> {
         _isLoading = true;
       });
       final user = FirebaseAuth.instance.currentUser;
-     /* FocusScope.of(context).unfocus();
-      final ref = FirebaseStorage.instance
-          .ref()
-          .child('user_image')
-          .child(user.uid + '.jpg');
-      await ref.putFile(
-        File(_userImageFile.path),
-      );*/
-      final userData = await FirebaseFirestore.instance
-          .collection('users')
-          .doc(user.uid)
-          .get();
-
-     // final url = await ref.getDownloadURL();
       await FirebaseFirestore.instance
           .collection('usersData')
           .doc(widget.documentId)
           .update({
         'title': _enteredTitle,
         'price': _enteredPrice,
-        'address': _enteredAddress,
         'createdAt': Timestamp.now(),
         'userId': user.uid,
-        //'username': userData['username'],
-        //'imageLink': url,
-        // 'userImage': userData['image_url'],
       });
       setState(() {
         _isLoading = false;
@@ -123,6 +106,7 @@ class _EditPostState extends State<EditPost> {
               child: Column(
                 children: [
                   TextFormField(
+
                     initialValue: widget.docList['title'],
                     onSaved: (value) {
                       _enteredTitle = value;
@@ -134,6 +118,7 @@ class _EditPostState extends State<EditPost> {
                       return null;
                     },
                     decoration: InputDecoration(
+                      hintStyle: myHintStyle,
                       hintText: "Enter title",
                       border: InputBorder.none,
                       contentPadding: EdgeInsets.only(left: 10),
@@ -168,40 +153,10 @@ class _EditPostState extends State<EditPost> {
                   return null;
                 },
                 decoration: InputDecoration(
+                  hintStyle: myHintStyle,
                     hintText: "Enter Price",
                     border: InputBorder.none,
                     contentPadding: EdgeInsets.only(left: 10)),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Neumorphic(
-              style: NeumorphicStyle(
-                shape: NeumorphicShape.concave,
-                boxShape: NeumorphicBoxShape.roundRect(
-                  BorderRadius.circular(12),
-                ),
-                depth: -4,
-                lightSource: LightSource.topLeft,
-//                    color: Colors.grey
-              ),
-              child: TextFormField(
-                initialValue: widget.docList['address'],
-                onSaved: (value) {
-                  _enteredAddress = value;
-                },
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Address can\'t be empty';
-                  }
-                  return null;
-                },
-                decoration: InputDecoration(
-                  hintText: "Enter Address",
-                  border: InputBorder.none,
-                  contentPadding: EdgeInsets.only(left: 10, top: 10, bottom: 10),
-                ),
               ),
             ),
             SizedBox(
